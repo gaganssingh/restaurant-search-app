b2a71f4ad2e07209a820b1630c7f005a546b26b4
Gagan Singh <gagansingh.dev@outlook.com>
2020-09-17 17:33:24 -0400

    Implemented Search Bar styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const SearchBar = () => {
     return (
-        <View style={styles.background}>
-            <Text>Search Bar</Text>
+        <View style={styles.backgroundStyle}>
+            <Ionicons style={styles.iconStyles} name="ios-search" />
+            <TextInput style={styles.inputStyle} placeholder="Search" />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    background: {
-        backgroundColor: "#ccc",
+    backgroundStyle: {
+        marginTop: 10,
+        backgroundColor: "#e5e6e4",
         height: 50,
         borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: "row",
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18,
+    },
+    iconStyles: {
+        fontSize: 35,
+        alignSelf: "center",
         marginHorizontal: 15,
     },
 });
